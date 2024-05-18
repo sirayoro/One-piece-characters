e4e9219ee28dbe2ca8fdb44b2721f089d1fe5a13
Sirayoro <ilyagsxr600@gmail.com>
2024-05-18 07:49:15 +0500

    Try to fix comments

--- One-piece/End_point_create_analytics.py
+++ One-piece/End_point_create_analytics.py
@@ -4,12 +4,16 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Подключение к базе данных SQLite
+# Коннект к бд
 def get_db_connection():
     conn = sqlite3.connect('one_piece.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+"""
+Запрос на получение всех персонажей из бд
+ http://localhost:5000/analytics/total_characters
+"""
 @app.route('/analytics/total_characters', methods=['GET'])
 def total_characters():
     conn = get_db_connection()
@@ -19,6 +23,10 @@ def total_characters():
     conn.close()
     return jsonify({'total_characters': total})
 
+"""
+ Запрос на получение топ 5 персонажей по награде
+ http://localhost:5000/analytics/top_bounties
+"""
 @app.route('/analytics/top_bounties', methods=['GET'])
 def top_bounties():
     conn = get_db_connection()
@@ -29,6 +37,10 @@ def top_bounties():
     top_bounties = [{'name': row['name'], 'bounty': row['bounty']} for row in characters]
     return jsonify({'top_bounties': top_bounties})
 
+""""
+ Запрос на получение среднего возраста персонажей
+ http://localhost:5000/analytics/average_age
+"""
 @app.route('/analytics/average_age', methods=['GET'])
 def average_age():
     conn = get_db_connection()
@@ -38,6 +50,10 @@ def average_age():
     conn.close()
     return jsonify({'average_age': avg_age})
 
+"""
+# Запрос на вывод персонажей, съевших дьявольский фрукт
+# http://localhost:5000/analytics/devil_fruit_users
+"""
 @app.route('/analytics/devil_fruit_users', methods=['GET'])
 def devil_fruit_users():
     conn = get_db_connection()
