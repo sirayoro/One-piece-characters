7edb604989dc0f623fa8fd5770203c74641f1e23
sirayoro <142376834+sirayoro@users.noreply.github.com>
2024-05-21 07:38:30 +0500

    mini upd

--- OnePiece-characters-data-pipeline/no_debug_modules/db_fast_check.py
+++ OnePiece-characters-data-pipeline/no_debug_modules/db_fast_check.py
@@ -1,19 +1,13 @@
 import sqlite3
 
-# Подключение к базе данных SQLite
 conn = sqlite3.connect('one_piece.db')
 cur = conn.cursor()
 
-# Выполнение запроса для выборки всех значений из таблицы characters
 cur.execute('SELECT * FROM characters')
-
-# Получение всех строк результата
 rows = cur.fetchall()
 
-# Вывод результатов
 for row in rows:
     print(row)
 
-# Закрытие подключения
 cur.close()
 conn.close()
